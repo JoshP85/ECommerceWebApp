4c113ed85ab5fac1ceaf4c5acacac8cef35b5d9b
JoshP85 <joshua.phillips1985@gmail.com>
2023-01-18 16:46:26 +1100

    Cleans up code in account repo

--- ECommerceWebApp/Data/AccountRepository.cs
+++ ECommerceWebApp/Data/AccountRepository.cs
@@ -12,35 +12,6 @@ namespace ECommerceWebApp.Data
             _context = context;
         }
 
-        /*public AccountRepository(DatabaseContext context)
-        {
-            _context = context;
-        }*/
-
-        /*        public void Add(Account user)
-                {
-                    _databaseContext.Accounts.Add(user);
-                }
-
-                public void Update(Account user)
-                {
-                    _databaseContext.Accounts.Update(user);
-                }
-
-                public void Delete(Account user)
-                {
-                    _databaseContext.Accounts.Remove(user);
-                }
-
-                public Account Get(int id)
-                {
-                    return _databaseContext.Accounts.Find(id);
-                }
-
-                public IEnumerable<Account> GetAll()
-                {
-                    return _databaseContext.Accounts.ToList();
-                }*/
         public async Task<Account> GetAccountByEmailAync(string email)
         {
             return await _context.Accounts.FirstOrDefaultAsync(_account => _account.Email == email);
