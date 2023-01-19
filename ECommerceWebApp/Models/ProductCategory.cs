3e66d5ed65370e142b095cb87c16a71d58559733
JoshP85 <joshua.phillips1985@gmail.com>
2023-01-19 15:55:32 +1100

    Product and Product Category models

---
+++ ECommerceWebApp/Models/ProductCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceWebApp.Models
+{
+    public class ProductCategory
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
